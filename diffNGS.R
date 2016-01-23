67273933bdf5db9d01424d44ffcbe01b8f0089e5
Pedro Madrigal <dnaseiseq@gmail.com>
2016-01-23 11:36:57 +0000

    Update diffNGS.R

--- diffNGS.R
+++ diffNGS.R
@@ -1,2 +1,3 @@
 # Modied version of function narrowpeaksDiff.R in Bioconductor package NarrowPeaks
+# (Shape-based Analysis of Variation in ChIP-seq using Functional PCA)
 # http://bioconductor.org/packages/devel/bioc/html/NarrowPeaks.html
