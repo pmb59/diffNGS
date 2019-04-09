2c28a2dd5b130fdde03174f55610a12c26aa4bf1
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2019-04-09 15:13:17 +0100

    Update example.R

--- example.R
+++ example.R
@@ -1,26 +1,25 @@
 ### Load the package or install it
-  if (!require("fda"))      { install.packages("fda") } 
-  if (!require("ICSNP"))    { install.packages("ICSNP") } 
-### BioC
-  source("http://bioconductor.org/biocLite.R")
-  if (!require("genomation"))     { biocLite("genomation")    } 
-  if (!require("GenomicRanges"))  { biocLite("GenomicRanges") } 
-  if (!require("RColorBrewer"))   { biocLite("RColorBrewer")  } 
-  
+### CRAN
+if (!require("fda"))      { install.packages("fda") } 
+if (!require("ICSNP"))    { install.packages("ICSNP") } 
+### Bioconductor
+source("http://bioconductor.org/biocLite.R")
+if (!require("genomation"))     { biocLite("genomation")    } 
+if (!require("GenomicRanges"))  { biocLite("GenomicRanges") } 
+if (!require("RColorBrewer"))   { biocLite("RColorBrewer")  } 
+
 #set working dir
 setwd(".../diffNGS")
-  
+
 ### Label your experimental conditions
-CNDS     <- c(rep("Wt",2), rep("Mt",2) )
+CNDS     <- c('S1','S1','S2','S2')
 ### Normalized bigwig files
-bws      <- c( "Wt_rep1.norm.bw", "Wt_rep2.norm.bw", "Mt_rep1.norm.bw", "Mt_rep2.norm.bw" ) 
+bws      <- c('S11.bw','S12.bw','S21.bw','S22.bw')
 ### Aggregated list of peak regions after peak calling against a paired control (in BED format)
-peaks    <-  "peaks.bed" 
-    
+peaks    <-  'regions.bed'
+
 ### Parameters of the Analysis
-adjP <- 1e-6    # Bonferroni adjusted p-value cuttoff
-AbsFc <- 2      # absolute fold change cutoff
-#Fl <- 1000      # flanks ( Fl bp upstream and dowstream ) around region center to use in the analysis
+Fl <- 0         # flanks ( Fl bp upstream and dowstream ) around region center to use in the analysis
 Nbasis <- 10    # number of B-spline basis used in the functiona PCA analysis
 Bins <- 31      # Number of bins in genomation
 
@@ -28,16 +27,39 @@ Bins <- 31      # Number of bins in genomation
 bed <- readGeneric(peaks, keep.all.metadata = FALSE)
 
 #Create data.frame structure to store results
-results <- data.frame(region.chr=seqnames(bed), region.start=start(bed)-Fl ,region.end=end(bed)+Fl , condition_C2vsC1=  paste(rev(unique(CNDS)), collapse="_vs_"), avg.C2= rep(NA,length(bed)), avg.C1= rep(NA,length(bed)), pval=rep(NA,length(bed)),  Bonferroni.pval=rep(NA,length(bed)),  fc= rep(NA,length(bed)) ,  diff= rep(FALSE,length(bed)), diff.score= rep(NA,length(bed))     )
+results <- data.frame(region.chr=seqnames(bed), region.start=start(bed)-Fl ,region.end=end(bed)+Fl , condition_C2vsC1=  paste(rev(unique(CNDS)), collapse="_vs_"), avg.C2= rep(NA,length(bed)), avg.C1= rep(NA,length(bed)), pval=rep(NA,length(bed)),  fdr=rep(NA,length(bed)),  log2fc= rep(NA,length(bed))     )
+head(results)
 
 # Run diffNGS
 source("diffNGS.R")  
-x <- diffNGS(bedFile= peaks , headerBed=FALSE, bigwigs=bws, conditions=CNDS, pcs = 2, variation = 0.01, nbasis=Nbasis, NB=Bins)
+x <- diffNGS(bedFile= peaks , headerBed=FALSE, bigwigs=bws, conditions=CNDS, pcs = 2, variation = 0.3, nbasis=Nbasis, NB=Bins)
+head(x)
 
-source("processDiffNGS.R")
-processDiffNGS(rawPvals = x$p.values, results=results, CNDS = CNDS, AbsFc = AbsFc, adjP = adjP, plotpdf=TRUE, Xmin=-5, Xmax=5, GTF="genes_ensembl_76_transcriptome-GRCh38_15.gtf", W=5e3 )
-#Processed GTF:
-#cat ensembl_76_transcriptome-GRCh38_15.gtf  | awk '$3 == "gene" {print $0}' > genes_ensembl_76_transcriptome-GRCh38_15.gtf
-    
+#Obtain P.values and Fold-changes
+for (j in 1:length(x$p.values)  ){
 
+  results$pval[j] <- x$p.values[[j]][1,2]
+  
+  results$avg.C2[j] <- mean( max(x$fdaprofiles[[3]][j,]) , max(x$fdaprofiles[[4]][j,]) ) 
+  results$avg.C1[j] <- mean( max(x$fdaprofiles[[1]][j,]) , max(x$fdaprofiles[[2]][j,]) ) 
+  #get the FCs
+  if ( results$avg.C2[j] >= results$avg.C1[j]) {  results$log2fc[j]   <-   log2(  ( results$avg.C2[j] + 0.001) / ( results$avg.C1[j] + 0.001  )  )  }    # Increase
+  if ( results$avg.C2[j] <  results$avg.C1[j]) {  results$log2fc[j]   <-   log2(  ( results$avg.C1[j] + 0.001) / ( results$avg.C2[j]  + 0.001 )  )  }    # Decrease
+  
+}
+
+#as.numeric(noquote(unlist(format(.Machine)))[1])
+zeroP <- which(results$pval == 0.0)
+results$pval[zeroP] <- as.numeric(noquote(unlist(format(.Machine)))[1])
+
+#Correct P-values for multiple hypothesis testing
+results$fdr <- p.adjust(results$pval, method = "fdr")
+head(results)
+
+# Sort results
+results_sorted  <- results[order(results$fdr, decreasing = FALSE),] 
+head(results_sorted )
+
+#write results
+write.csv(results_sorted , file='results_sorted.csv', row.names = F)
 
